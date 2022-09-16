f3b7a3f3f79ff47e8dbb0d2833d608a49c78f9a7
Olivier Peyrusse <ope@activeviam.com>
2022-09-16 09:55:49 +0200

    Publicly expose ExecError.
    This can be useful in case of error to inspect the command outputs.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Command, getCommand, OptionsContext } from "./commands.js";
 export { Context } from "./context.js";
-export { Exec } from "./exec.js";
+export { Exec, ExecError } from "./exec.js";
 export { Task } from "./tasks.js";

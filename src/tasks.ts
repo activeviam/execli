3f2739ca3eadf0863ac56e318068b8e22d383bfe
Thibault Derousseaux <6574550+tibdex@users.noreply.github.com>
2022-09-19 18:26:30 -0400

    Fix dependencies upgrade (#30)

--- src/tasks.ts
+++ src/tasks.ts
@@ -229,7 +229,7 @@ const buildStaticallySkippedTasks = (
     staticallySkippedTasks[taskTitle] = "only";
   } else if (
     context.tag.length > 0 &&
-    context.tag.every((givenTag) => (task?.tags ?? []).includes(givenTag))
+    context.tag.every((givenTag) => !(task?.tags ?? []).includes(givenTag))
   ) {
     staticallySkippedTasks[taskTitle] = "tag";
   }
